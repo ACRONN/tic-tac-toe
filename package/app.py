27e710091e65693878ad0b17538560bcc7652a02
ACRONN <40163450+ACRONN@users.noreply.github.com>
2022-11-10 12:35:46 +1100

    Implement basic game with invalid coordinate check

--- package/app.py
+++ package/app.py
@@ -1,16 +1,32 @@
-
-
-def userinput(array):
+def game(array):
     turn = 1
     score = ["O","X"]
-    current_player = "a"
-    while True:
+    current_player = ""
+    win = False
+    while win is False:
         current_player = score[turn%2]
-        uinput = input("Player "+ str(turn%2)+ " enter your move 'row collumn'")
-        row = uinput.split()[0]
-        collumn =uinput.split()[1]
-        array[int(row)][int(collumn)] = current_player
-        turn +=1
+        while True:
+            try:
+                if turn % 2 == 0:
+                    uinput = input("Player "+ str((turn % 2) + 2)+ " enter your move ('row column'): ")
+                else:
+                    uinput = input("Player "+ str(turn % 2)+ " enter your move ('row column'): ")
+                row = uinput.split()[0]
+                column = uinput.split()[1]
+                array[int(row)][int(column)] = current_player
+                break
+            except IndexError:
+                print("Invalid coordinates.\n")
+                continue
+
+        # Need to implement win condition.
+
+        turn += 1
         for i in range(len(array)):
             print(array[i])
 
+def createArray(rows,cols):
+    arr = [["-" for i in range(cols)] for j in range(rows)]
+    return arr
+
+game(createArray(3,3))
