aeb4fbe2e0edaf6b18e8cc530cec03e311d6b959
ACRONN <40163450+ACRONN@users.noreply.github.com>
2022-11-14 09:19:31 +1100

    Implement input validation for player coordinates

--- package/app.py
+++ package/app.py
@@ -1,3 +1,6 @@
+maxRow = 2
+maxColumn = 2
+
 def game(array):
     turn = 1
     score = ["O","X"]
@@ -7,16 +10,21 @@ def game(array):
         current_player = score[turn%2]
         while True:
             try:
-                if turn % 2 == 0:
-                    uinput = input("Player "+ str((turn % 2) + 2)+ " enter your move ('row column'): ")
-                else:
-                    uinput = input("Player "+ str(turn % 2)+ " enter your move ('row column'): ")
-                row = uinput.split()[0]
-                column = uinput.split()[1]
+                while True:
+                    if turn % 2 == 0:
+                        uinput = input("Player "+ str((turn % 2) + 2)+ " enter your move ('row column'): ")
+                    else:
+                        uinput = input("Player "+ str(turn % 2)+ " enter your move ('row column'): ")
+                    row = uinput.split()[0]
+                    column = uinput.split()[1]
+                    if int(row) > maxRow or int(column) > maxColumn:
+                        print("Invalid coordinates. Range cannot exceed 2 2.\n")
+                        continue
+                    break
                 array[int(row)][int(column)] = current_player
                 break
             except IndexError:
-                print("Invalid coordinates.\n")
+                print("Invalid coordinates. Please enter two integers separated by a space.\n")
                 continue
 
         # Need to implement win condition.
